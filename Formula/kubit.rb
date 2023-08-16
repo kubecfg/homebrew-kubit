9262c36fa4fa37928a992b867447b60a4d213b2f
Marko Mikulicic <mmikulicic@gmail.com>
2023-08-16 08:28:29 +0200

    Initial

---
+++ Formula/kubit.rb
@@ -0,0 +1,14 @@
+class Kubit < Formula
+  desc "Kubecfg kubit operator"
+  homepage "https://github.com/kubecfg/kubit"
+  url "https://github.com/kubecfg/kubit/archive/v0.0.6.tar.gz"
+  sha256 "60683f380680b942fec3bee11ff36e8e707844e6eb5407f61e0368c00fa0137f"
+  license "Apache-2.0"
+  head "https://github.com/kubecfg/kubit.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
