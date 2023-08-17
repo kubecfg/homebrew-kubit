4b8ac2d15c0ac7a00ca06d2599adf4538cd67527
Marko Mikulicic <mmikulicic@gmail.com>
2023-08-17 19:11:43 +0200

    Update to 0.0.8

--- Formula/kubit.rb
+++ Formula/kubit.rb
@@ -1,8 +1,8 @@
 class Kubit < Formula
   desc "Kubecfg kubit operator"
   homepage "https://github.com/kubecfg/kubit"
-  url "https://github.com/kubecfg/kubit/archive/v0.0.7.tar.gz"
-  sha256 "93f248a2ca864b0ef2bab3583809082b7e7e0ea4e4271469f0f240d0bd861d06"
+  url "https://github.com/kubecfg/kubit/archive/v0.0.8.tar.gz"
+  sha256 "e46a339553637be09e4b87c162fdbd74757fa9f125f0dc2ae723b7fdf7b27f7f"
   license "Apache-2.0"
   head "https://github.com/kubecfg/kubit.git", branch: "main"
 
