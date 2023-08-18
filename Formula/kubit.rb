6d62fffae68fdb6a762706435832a4b4869bd99b
Marko Mikulicic <mmikulicic@gmail.com>
2023-08-19 00:44:56 +0200

    Fix sha

--- Formula/kubit.rb
+++ Formula/kubit.rb
@@ -2,7 +2,7 @@ class Kubit < Formula
   desc "Kubecfg kubit operator"
   homepage "https://github.com/kubecfg/kubit"
   url "https://github.com/kubecfg/kubit/archive/v0.0.9.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "6a7c2371dc1b18b6c3d45a5a06503982492b0b95ef677050bf9acf5c7c473ba8"
   license "Apache-2.0"
   head "https://github.com/kubecfg/kubit.git", branch: "main"
 
